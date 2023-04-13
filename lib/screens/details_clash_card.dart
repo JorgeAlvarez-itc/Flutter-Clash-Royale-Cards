96b069d80dad77758dbee0e7f2bdf9d2e2879ed6
Jorge <19030136@itcelaya.edu.mx>
2023-04-12 20:03:43 -0600

    Responsive views add

--- lib/screens/details_clash_card.dart
+++ lib/screens/details_clash_card.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import '../responsive.dart';
 import 'package:flutter/material.dart';
 import '../widgets/fadein_widget.dart';
 import '../models/card_clash_model.dart';
@@ -43,88 +44,191 @@ class _CardClashDetailsScreenState extends State<CardClashDetailsScreen> {
               ),
             ),
           ),
-          Center(
-            child: Hero(
-              tag: 'card_${widget.card.id}',
-              child: FadeIn(
-                child: Card(
-                  elevation: 8,
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 96),
-                  child: Padding(
-                    padding: const EdgeInsets.all(1),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AspectRatio(
-                          aspectRatio: 12 / 8,
-                          child: InteractiveViewer(
-                            child: CachedNetworkImage(
-                              imageUrl: widget.card.iconUrls ?? '',
-                              placeholder: (context, url) => Center(
-                                child: LoadingAnimationWidget.dotsTriangle(
-                                  color: Colors.blueAccent,
-                                  size: 30,
-                                ),
-                              ),
-                              errorWidget: (context, url, error) =>
-                                  Icon(Icons.error),
-                            ),
-                          ),
-                        ),
-                        Divider(),
-                        ListTile(
-                          leading: Icon(Icons.person),
-                          title: Text(
-                            'Name',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Animate(
-                              delay: 400.ms,
-                              effects: [
-                                FadeEffect(duration: 2000.ms),
-                              ],
-                              child: Text(widget.card.name ?? '')),
-                        ),
-                        Divider(),
-                        ListTile(
-                          leading: Icon(Icons.confirmation_number),
-                          title: Text(
-                            'ID',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Animate(
-                              delay: 500.ms,
-                              effects: [
-                                FadeEffect(duration: 2000.ms),
-                              ],
-                              child: Text(widget.card.id.toString())),
-                        ),
-                        Divider(),
-                        ListTile(
-                          leading: Icon(Icons.arrow_upward),
-                          title: Text(
-                            'Max Level',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Animate(
-                              delay: 600.ms,
-                              effects: [
-                                FadeEffect(duration: 2000.ms),
-                              ],
-                              child: Text(widget.card.maxLevel.toString())),
-                        ),
-                        Spacer(),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
+          Responsive(
+            mobile: _buildMobileConten(context),
+            tablet: _buildTabletContent(context),
+            desktop: _buildTabletContent(context),
           ),
         ],
       ),
     );
   }
+
+  Widget _buildMobileConten(BuildContext context) {
+    return Center(
+      child: Hero(
+        tag: 'card_${widget.card.id}',
+        child: FadeIn(
+          child: Card(
+            elevation: 8,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 96),
+            child: Padding(
+              padding: const EdgeInsets.all(1),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 12 / 8,
+                    child: InteractiveViewer(
+                      child: CachedNetworkImage(
+                        imageUrl: widget.card.iconUrls ?? '',
+                        placeholder: (context, url) => Center(
+                          child: LoadingAnimationWidget.dotsTriangle(
+                            color: Colors.blueAccent,
+                            size: 30,
+                          ),
+                        ),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text(
+                      'Name',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Animate(
+                        delay: 400.ms,
+                        effects: [
+                          FadeEffect(duration: 2000.ms),
+                        ],
+                        child: Text(widget.card.name ?? '')),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.confirmation_number),
+                    title: Text(
+                      'ID',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Animate(
+                        delay: 500.ms,
+                        effects: [
+                          FadeEffect(duration: 2000.ms),
+                        ],
+                        child: Text(widget.card.id.toString())),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.arrow_upward),
+                    title: Text(
+                      'Max Level',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Animate(
+                        delay: 600.ms,
+                        effects: [
+                          FadeEffect(duration: 2000.ms),
+                        ],
+                        child: Text(widget.card.maxLevel.toString())),
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTabletContent(BuildContext context){
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Card(
+            elevation: 8,
+            margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            child: Padding(
+              padding: const EdgeInsets.all(0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 12 / 8,
+                    child: InteractiveViewer(
+                      child: CachedNetworkImage(
+                        imageUrl: widget.card.iconUrls ?? '',
+                        placeholder: (context, url) => Center(
+                          child: LoadingAnimationWidget.dotsTriangle(
+                            color: Colors.blueAccent,
+                            size: 30,
+                          ),
+                        ),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
+                      ),
+                    ),
+                  )
+                ]
+              )
+            )
+          ),
+        ),
+        Expanded(
+          flex:2,
+          child: Card(
+            elevation: 8,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 26),
+            child: Padding(
+              padding: const EdgeInsets.all(0),
+              child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.person),
+                        title: Text(
+                          'Name',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Animate(
+                            delay: 400.ms,
+                            effects: [
+                              FadeEffect(duration: 2000.ms),
+                            ],
+                            child: Text(widget.card.name ?? '')),
+                      ),
+                      Divider(),
+                      ListTile(
+                        leading: Icon(Icons.confirmation_number),
+                        title: Text(
+                          'ID',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Animate(
+                            delay: 500.ms,
+                            effects: [
+                              FadeEffect(duration: 2000.ms),
+                            ],
+                            child: Text(widget.card.id.toString())),
+                      ),
+                      Divider(),
+                      ListTile(
+                        leading: Icon(Icons.arrow_upward),
+                        title: Text(
+                          'Max Level',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Animate(
+                            delay: 600.ms,
+                            effects: [
+                              FadeEffect(duration: 2000.ms),
+                            ],
+                            child: Text(widget.card.maxLevel.toString())),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
 }
